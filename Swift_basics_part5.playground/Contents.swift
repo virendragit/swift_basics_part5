17624d2f548b83b06ef98e703014e275acb86562
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-11 14:39:07 +0530

    added code for set and dictionarey

--- Swift_basics_part5.playground/Contents.swift
+++ Swift_basics_part5.playground/Contents.swift
@@ -119,3 +119,136 @@ letters = []
 
 var favorite: Set<String> = ["Rock", "Classical", "HipHop"]
 
+// A set type cannot be inferred from an array literal alone, so the type set must be explicitly decleared.
+
+var favoriteGenres: Set = ["Rock", "Classical", "Hip hop"]
+
+//var favoriteGenres: Set = Set<String>()
+//empty set
+
+//var results = [Int]()
+//empty array
+
+
+print("I have \(favoriteGenres.count) favorite music genres.")
+
+if favoriteGenres.isEmpty {
+    print("As far as music goes, I'm not picky.")
+} else {
+    print("I have particular music preferences.")
+}
+
+
+favoriteGenres.insert("Jazz")
+print(favoriteGenres)
+
+
+//remove
+if let removeGenere = favoriteGenres.remove("Rock"){
+    print("\(removeGenere)? I'm over it.")
+} else{
+    print("Not yet over")
+}
+
+//check whether a set contains a particular item
+if favoriteGenres.contains("funk") {
+    print("hterr")
+}
+else{
+    print("not ther")
+}
+
+favoriteGenres.insert("Rock")
+
+//iterating over a set
+for genre in favoriteGenres.sorted() {
+    print("\(genre)")
+}
+
+//Fundamental set operations.
+
+let oddDigits: Set = [1,3,5,7,9]
+let evenDigits: Set = [0,2,4,6,8]
+let sigleDigitPrimeNumbers : Set = [2,3,5,7]
+
+//union
+oddDigits.union(evenDigits).sorted()
+
+//intersection
+oddDigits.intersection(evenDigits).sorted()
+
+oddDigits.subtracting(sigleDigitPrimeNumbers).sorted()
+
+oddDigits.symmetricDifference(sigleDigitPrimeNumbers).sorted()
+
+//set membership and equality
+let houseAnimals: Set = ["🐶", "🐱"]
+let farmAnimals: Set = ["🐮", "🐔", "🐑", "🐶", "🐱"]
+let cityAnimals: Set = ["🐦", "🐭"]
+
+houseAnimals.isSubset(of: farmAnimals)
+farmAnimals.isSuperset(of: houseAnimals)
+farmAnimals.isDisjoint(with: cityAnimals)
+
+
+//Dictionary
+//Dictionary<Key, Value>
+
+//creating empty dictionary
+
+var nameOfIntegers = [Int: String]()
+nameOfIntegers[16] = "sixteen"
+nameOfIntegers = [:]
+
+var airports:[String: String] = ["YYZ": "Toronto", "DUB": "Dublin" ]
+print("The airports dictionary contains \(airports.count) items.")
+
+if airports.isEmpty {
+    print("The airports dictionary is empty.")
+} else {
+    print("The airports dictionary is not empty.")
+}
+
+airports["LHR"] = "London"
+print("The airports dictionary contains \(airports.count) items.")
+
+airports["LHR"] = "London Heathrow"
+print("The airports dictionary contains \(airports.count) items.")
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue).")
+}
+
+if let airportName = airports["DUB"] {
+    print("The name of the airport is \(airportName).")
+} else {
+    print("That airport is not in the airports dictionary.")
+}
+
+
+//airports["DUB"] = nil
+
+if let removedValue = airports.removeValue(forKey: "DUB") {
+    print("The removed airport's name is \(removedValue).")
+} else {
+    print("The airports dictionary does not contain a value for DUB.")
+}
+
+
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
+
+for airportCode in airports.keys {
+    print("Airport code::\(airportCode)")
+}
+
+for airportName in airports.values {
+    print("Airport name::\(airportName)")
+}
+
+let airportCodes = [String](airports.keys)
+
+let airportNames = [String](airports.values)
+
+
